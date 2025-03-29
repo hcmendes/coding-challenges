ad1632076ab7554da3be1a4d3afbc32408a8656e
hcmendes <hilton.m.cardoso@gmail.com>
2025-03-29 14:42:51 -0300

    rename

---
+++ MergeSortCountingInversions.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+* @author github.com/hcmendes
+*/
+class Result {
+
+    /*
+     * Complete the 'countInversions' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static long countInversions(List<Integer> arr) {
+        // Write your code here
+        return mergeSortAndCount(arr.stream().mapToInt(Integer::intValue).toArray(), 0, arr.size() - 1);
+    }
+    
+    public static long mergeSortAndCount(int[] arr, int left, int right) {
+        if (left >= right) return 0;
+        
+        int mid = (left + right) / 2;
+        long count = 0;
+        
+        count += mergeSortAndCount(arr, left, mid);
+        count += mergeSortAndCount(arr, mid + 1, right);
+        count += mergeAndCount(arr, left, mid, right);
+        
+        return count;
+    }
+
+    private static long mergeAndCount(int[] arr, int left, int mid, int right) {
+        int[] leftArr = Arrays.copyOfRange(arr, left, mid + 1);
+        int[] rightArr = Arrays.copyOfRange(arr, mid + 1, right + 1);
+        
+        int i = 0;
+        int j = 0;
+        int k = left;
+        long swaps = 0;
+        
+        while (i < leftArr.length && j < rightArr.length) {
+            
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+                swaps += (mid + 1) - (left + i);
+            }
+        }
+        
+        while (i < leftArr.length) {
+            arr[k++] = leftArr[i++];
+        }
+        
+        while(j < rightArr.length) {
+            arr[k++] = rightArr[j++];
+        }
+        
+        return swaps;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                long result = Result.countInversions(arr);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
